b9128965b34b4559a2304d8a5161947ac0fbe01a
pethmi4@gmail.com <pethmi4@gmail.com>
2024-06-08 21:00:43 +0530

    database functions changes

--- backend/server.js
+++ backend/server.js
@@ -107,7 +107,7 @@ app.post("/addStudent", async function (req, res) {
     reqBody.id,
     reqBody.name,
     reqBody.age,
-    reqBody.religion
+    reqBody.hometown
   );
 
   res.setHeader("Content-Type", "application/json");
@@ -130,7 +130,7 @@ app.post("/editStudent", async function (req, res) {
   console.log(
     "Request received to update Student. Req body: " + JSON.stringify(reqBody)
   );
-  let data = await updateStudent(reqBody.name,reqBody.age,reqBody.religion,reqBody.id);
+  let data = await updateStudent(reqBody.name,reqBody.age,reqBody.hometown,reqBody.id);
 
   res.setHeader("Content-Type", "application/json");
   res.end(JSON.stringify(data));
